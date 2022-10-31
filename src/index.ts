d0be460b3bc2fdfe95b8eb06e3fa579af1863daf
Rupert Foggo McKay <maxfmckay@gmail.com>
2022-10-31 12:04:24 +0100

    ⬅️ Display previous result feedback

--- src/index.ts
+++ src/index.ts
@@ -19,6 +19,16 @@ const answerA = document.querySelector("#A")!;
 const answerB = document.querySelector("#B")!;
 const answerC = document.querySelector("#C")!;
 const answerD = document.querySelector("#D")!;
+const previousResult = document.querySelector("#previousResult")!;
+const ready = document.querySelector("#ready")!;
+const box1 = document.querySelector("#box1")!;
+const box2 = document.querySelector("#box2")!;
+const box3 = document.querySelector("#box3")!;
+const box4 = document.querySelector("#box4")!;
+const box5 = document.querySelector("#box5")!;
+const box6 = document.querySelector("#box6")!;
+const box7 = document.querySelector("#box7")!;
+const retired = document.querySelector("#retired")!;
 
 let state: GameState;
 try {
@@ -41,16 +51,45 @@ const updateUI = () => {
 	answerB.textContent = answers[1].text;
 	answerC.textContent = answers[2].text;
 	answerD.textContent = answers[3].text;
+	ready.textContent = state.ready.length.toString();
+	box1.textContent = state.box1.length.toString();
+	box2.textContent = state.box2.length.toString();
+	box3.textContent = state.box3.length.toString();
+	box4.textContent = state.box4.length.toString();
+	box5.textContent = state.box5.length.toString();
+	box6.textContent = state.box6.length.toString();
+	box7.textContent = state.box7.length.toString();
+	retired.textContent = state.retired.length.toString();
 };
 
 updateUI();
 
+const positiveFeedback = [
+	"Nice!",
+	"Good job!",
+	"You're the best!",
+	"Keep it up!",
+	"You're practically Dr. Braille at this point!",
+	"Amazing!",
+	"Bravo!",
+	"YESSSS",
+	"Well done!",
+	"Got it right!",
+];
+
+const pickRandom = <T>(items: T[]): T =>
+	items[Math.floor(Math.random() * items.length)];
+
 [answerA, answerB, answerC, answerD].forEach((answer, i) =>
 	answer.addEventListener("click", () => {
 		// If this was correct?
-		state = answers[i].isCorrect
-			? markCardCorrect(state)
-			: markCardIncorrect(state);
+		if (answers[i].isCorrect) {
+			previousResult.textContent = pickRandom(positiveFeedback);
+			state = markCardCorrect(state);
+		} else {
+			previousResult.textContent = `Naw. ${state.currentCard.front} is actually ${state.currentCard.back}`;
+			state = markCardIncorrect(state);
+		}
 		// Persist
 		window.localStorage.setItem("state", stateToString(state));
 		// Update UI
